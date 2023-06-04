9872914962d76a5d509a12f08e447201f8e91be3
aaxpost <aaxpost@gmail.com>
2023-06-04 22:16:02 +0300

    4062023

---
+++ scripts/btn.js
@@ -0,0 +1,18 @@
+"use strict";
+//Это не мой код, нашел в сети (Alex Kh.)
+//Get the video
+let video = document.getElementById("myVideo");
+
+// Get the button
+let btn = document.getElementById("myBtn");
+
+// Pause and play the video, and change the button text
+function myFunction() {
+    if (video.paused) {
+        video.play();
+        //btn.innerHTML = "Pause";
+    } else {
+        video.pause();
+        //btn.innerHTML = "Play";
+    }
+}
